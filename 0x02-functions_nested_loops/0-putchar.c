106d2205b6568809921f506729cc76fb2c7c050f
Ajibade Yakub Adebare <ajibadeyakubadebare@gmail.com>
2022-12-14 08:01:18 -0800

    fufviy

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,4 +1,3 @@
-#include <stdio.h>
 #include <main.h>
 /**
  * main - Entry point
@@ -7,7 +6,16 @@
  */
 int main(void)
 {
-	printf("_putchar\n");
+	_putchar('_')
+	_putchar('p')
+	_putchar('u')
+	_putchar('t')
+	_putchar('c')
+	_putchar('h')
+	_putchar('a')
+	_putchar('r')
+	-putchar('\n')
+
 
 	return (0);
 }
