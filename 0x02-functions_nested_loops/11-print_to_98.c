8d3624bb5c3c3097fb4fb87b55dda55d5519bf1f
Ajibade Yakub Adebare <ajibadeyakubadebare@gmail.com>
2022-12-14 13:31:19 -0800

    ksf

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 /**
- * add - Entry point
+ * print_to_98 - Entry point
  *
  * @n: the fist number
  * Return: Return the result
@@ -14,7 +14,7 @@ void print_to_98(int n)
 			printf("%d, ", n);
 		printf("%d\n", 98);
 	}
-	else (n > 98)
+	else
 	{
 		for (n = n; n > 98; n--)
 			printf("%d, ", n);
