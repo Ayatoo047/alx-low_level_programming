2c180ffad50c746c01de4d6466dafe71467ece9a
Ajibade Yakub Adebare <ajibadeyakubadebare@gmail.com>
2022-12-14 11:34:24 -0800

    try

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * add - Entry point
+ *
+ * @n: the fist number
+ * Return: Return the result
+ */
+
+void print_to_98(int n)
+{
+	for (n = n; n <= 98; n++)
+		_putchar(n);
+}
