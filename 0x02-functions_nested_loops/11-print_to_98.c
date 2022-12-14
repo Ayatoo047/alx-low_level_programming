f7cc157280f46f3a715855bc524cedcef0c1a9c5
Ajibade Yakub Adebare <ajibadeyakubadebare@gmail.com>
2022-12-14 11:45:43 -0800

    try

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -8,6 +8,16 @@
 
 void print_to_98(int n)
 {
-	for (n = n; n <= 98; n++)
-		_putchar(n);
+	if (n < 98)
+	{
+		for (n = n; n <= 98; n++)
+			printf("%d", n);
+		printf("\n")
+	}
+	else
+	{
+		for (n = n; n <= 98; n--)
+			printf(n);
+		print("\n");
+	}
 }
