6ba38b9227dff6546770d6485f0eb49fb2ad65fb
Ajibade Yakub Adebare <ajibadeyakubadebare@gmail.com>
2022-12-14 11:20:39 -0800

    try

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -3,16 +3,15 @@
 
 /**
  * _islower - check if a character is lower
- * c - the chracter
+ * @c: the chracter
  *
- * Return 0 for Upper
- * Return 1 for lower
+ * Return: 0 for Upper Return 1 for lower
  */
 
 int _islower(char c)
 {
 	if (islower(c) > 0)
-		return(1);
+		return (1);
 	else
-		return(0);
+		return (0);
 }
