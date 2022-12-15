09f95571229a762fdc59cea6d7a4404230747870
Ajibade Yakub Adebare <ajibadeyakubadebare@gmail.com>
2022-12-15 13:09:44 -0800

    hjhsds

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -11,20 +11,26 @@ int main(void)
 {
 	int i;
 
-	for (i = 0; i <= 100; i++)
+	for (i = 1; i <= 100; i++)
 	{
-		if (i % 5 == 0 && i % 3 == 0)
-			printf(" FizzBuzz");
-		else if (i % 3 == 0)
+		if (i % 3 == 0 && i % 5 != 0)
+		{
 			printf(" Fizz");
-		else if (i % 5 == 0)
+		} else if (i % 5 == 0 && i % 3 != 0)
+		{
 			printf(" Buzz");
-		else if (i == 1)
+		} else if (i % 3 == 0 && i % 5 == 0)
+		{
+			printf(" FizzBuzz");
+		} else if (i == 1)
+		{
 			printf("%d", i);
-		else
+		} else
+		{
 			printf(" %d", i);
+		}
 	}
-	print("\n");
+	printf("\n");
 
 	return (0);
 }
