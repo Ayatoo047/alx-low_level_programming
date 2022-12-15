d390fc1188145618f9581174b5363b54ce4a6859
Ajibade Yakub Adebare <ajibadeyakubadebare@gmail.com>
2022-12-15 12:44:52 -0800

    hjhsds

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * print_most_numbers - print the number 1 to 9
+ * print_most_numbers - print the number 0 to 9
  * except 2 and 4, then a new line
  * Return: Nothing
  */
@@ -10,11 +10,9 @@ void print_most_number(void);
 {
 	int i;
 
-	for (i = 0; i >= 9; i++)
+	for (i = 0; i < 9; i++)
 	{
-		if (i == 2 || i == 4)
-			continue;
-		else
+		if (i != 2 && i != 4)
 			_putchar(i + '0');
 	}
 	_putchar('\n');
