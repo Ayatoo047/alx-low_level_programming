2fbd325f8e38f0904ccc23606a33b10dba4e5ce2
Ajibade Yakub Adebare <ajibadeyakubadebare@gmail.com>
2022-12-19 13:44:19 -0800

    another

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -1,18 +1,23 @@
 #include "main.h"
-/**
- * print_rev - print reverse
- *
- * @s: the parameter
- * Return: always 0
- */
 
+/**
+ * print_rev - to print in reverse order
+ * @s: pointer to  int
+ *
+ * Return: Always return 0.
+ */
 void print_rev(char *s)
 {
 	int i;
 
-	for (i < strlen(&s); i <= 0; i--)
+	i = 0;
+	while (s[i] != '\0')
 	{
-		_putchar("%s", s[i]);
+		i++;
 	}
-	_putchar('\n');
+	for (i = i - 1; i >= 0; i--)
+	{
+		_putchar (s[i]);
+	}
+	_putchar ('\n');
 }
---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_rev - to print in reverse order
+ * @s: pointer to  int
+ *
+ * Return: Always return 0.
+ */
+void print_rev(char *s)
+{
+	int i;
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		if (a % 2 == 0)
+		_putchar (str[a]);
+	}
+	_putchar ('\n');
+}
