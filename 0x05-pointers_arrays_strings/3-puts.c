9be2dd7850c09c7546305cade3e2c3a1893033ee
Ajibade Yakub Adebare <ajibadeyakubadebare@gmail.com>
2022-12-20 12:11:25 -0800

    try

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,5 +1,5 @@
 #include "main.h"
-
+#include <stdio.h>
 /**
  * _puts - function to print
  * @str: strings to print
@@ -12,11 +12,11 @@ void _puts(char *str)
 	int m;
 	char *c;
 
-	c[] = str;
+	c = str;
 	for (m = 0; c[m]; m++)
 	{
-		_putchar(c[m]);
+		putchar(c[m]);
 	}
 
-	_putchar('\n');
+	putchar('\n');
 }
